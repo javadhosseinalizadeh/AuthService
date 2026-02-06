34cbb4dc136b2ed49488757ac9a4b31aec80c2e0
Javad <javad@example.com>
2026-02-06 21:28:20 +0330

    Add services to Program.cs

--- AuthService.Api/Program.cs
+++ AuthService.Api/Program.cs
@@ -1,23 +1,28 @@
+using AuthService.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AuthDbContext>(opt =>
+{
+    var cs = builder.Configuration.GetConnectionString("AuthDb");
+    opt.UseNpgsql(cs);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwaggerUI();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
